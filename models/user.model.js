0f5e9027243032869a7e03db04afc0febc35d7c1
nathan1512 <74864024+nathan1512@users.noreply.github.com>
2023-05-05 11:11:10 +0530

    5may2023 1111

--- models/user.model.js
+++ models/user.model.js
@@ -10,6 +10,7 @@ const userSchema = new Schema({
         required: [true, "userName can't be empty"],
         // @ts-ignore
         unique: true,
+        match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))git@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
     },
     password: {
         type: String,
