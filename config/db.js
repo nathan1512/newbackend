78ead08280e90498cadb6e38ac482532f59c33c6
nathan1512 <74864024+nathan1512@users.noreply.github.com>
2023-05-05 11:20:27 +0530

    addednewemailvalidator 1120

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // mongoose.set('debug', true);
 
 
-const connection = mongoose.createConnection(`mongodb+srv://sarthakoberoiofficial:password12345678@cluster0.itn3z44.mongodb.net/db5`).on('open',()=>{console.log("MongoDB Connected");}).on('error',()=>{
+const connection = mongoose.createConnection(`mongodb+srv://sarthakoberoiofficial:password12345678@cluster0.itn3z44.mongodb.net/db6`).on('open',()=>{console.log("MongoDB Connected");}).on('error',()=>{
     console.log("MongoDB Connection error");
 });
 
